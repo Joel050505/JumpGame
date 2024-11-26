a501f9a277a7d41bbe0298dc60dca7a9e80a22c8
Your Name <joel.kilic@icloud.com>
2024-11-26 20:21:14 +0100

    update with coins, mobil, characters,abilitys and highscore

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ const playAgainButton = document.querySelector("#playAgainBtn");
 const playBtn = document.querySelector("#play-btn");
 const startMenu = document.querySelector("#container");
 
-// Characters
+// Characters and character related stuff
 const characterButton = document.querySelector("#characters-btn");
 const characterMenu = document.querySelector("#character-container");
 const characterBackBtn = document.querySelector("#back-to-start-menu-button");
@@ -24,14 +24,51 @@ const zigZane = document.querySelector("#zig-zane");
 const jumpSound = document.getElementById("jump-sound");
 const deathsound = document.getElementById("death-sound");
 const newHighScoreSound = document.getElementById("new-high-score");
+const backGroundMusic = document.getElementById("background-music");
+
+// Chest and coins
+const openChest = document.getElementById("openChest");
+const closedChest = document.getElementById("closedChest");
+const chestBox = document.getElementById("chest-box");
+const chestContainer = document.getElementById("chest-container");
+const coins = document.getElementById("coins");
+
+const roboSamBuyButton = document.getElementById("price2");
+const zigZaneBuyButton = document.getElementById("price3");
 
 const scoreDisplay = document.getElementById("scoreDisplay");
-// triggerJump.style.animationPlayState = "paused";
 
 // Scores variables
 let currentScore = 0;
 let scoreInterVal;
 let updateScoreTime = 2000;
+
+function playBackGroundMusic(music) {
+  music.play();
+  music.volume = 0.05;
+  music.currentTime = 0;
+}
+const scoreDisplayText = document.getElementById("scoreDisplayText");
+
+// Storing the highscore in the local storage
+// Function that will check ur highscore
+function highScoreData(score) {
+  if (score > Number(localStorage.getItem("highScore"))) {
+    localStorage.setItem("highScore", score);
+    scoreDisplay.textContent = currentScore;
+    scoreDisplayText.textContent = "Your new highscore is: ";
+    console.log(scoreDisplay.textContent);
+    playSound(newHighScoreSound);
+    console.log("worked");
+  } else {
+    playSound(deathsound);
+    deathsound.playbackRate = 1.2;
+    deathsound.volume = 0.2;
+    scoreDisplayText.textContent = "Your score was: ";
+  }
+}
+
+// Function that will store coins
 
 function playSound(sound) {
   sound.play();
@@ -63,36 +100,156 @@ function upDateScore() {
 }
 
 // Character select menu, so that you can select a character
+// Logic for every character so you have to unlock 2 of them so the game will have a goal
+
+const characters = {
+  willieCharacter: { life: 1, coinsMultipliyer: 1, unlocked: true },
+  roboSamCharacter: {
+    life: 1,
+    coinsMultipliyer: 1.5,
+    cost: 350,
+    unlocked: JSON.parse(localStorage.getItem("roboSamUnlocked")) || false,
+  },
+  zigZaneCharacter: {
+    life: 2,
+    coinsMultipliyer: 1.75,
+    cost: 800,
+    unlocked: JSON.parse(localStorage.getItem("zigZaneUnlocked")) || false,
+  },
+};
+
+// Buy mechanism for characters so that players can buy characters and after they buy it it will update your gold
+
+function buyMechanism(buyButton, localStorageName, _characterName) {
+  buyButton.addEventListener("click", (e) => {
+    e.stopPropagation();
+    let yourCoins = Number(localStorage.getItem("coins"));
+    let cost = _characterName.cost;
+    if (yourCoins >= cost) {
+      localStorage.setItem("coins", yourCoins - cost);
+      console.log(yourCoins);
+      coins.textContent = localStorage.getItem("coins");
+      buyButton.textContent = "Unlocked";
+      localStorage.setItem(localStorageName, true);
+      _characterName.unlocked = true;
+    } else {
+      alert("Not enough coins");
+    }
+  });
+}
+
+buyMechanism(roboSamBuyButton, "roboSamUnlocked", characters.roboSamCharacter);
+buyMechanism(zigZaneBuyButton, "zigZaneUnlocked", characters.zigZaneCharacter);
+
+// Logic for every character so you have to unlock 2 of them so the game will have a goal
+
 characterMenu.classList.add("displayNone");
 
-willieMenuImg.addEventListener("click", () => {
-  roboSam.style.display = "none";
-  willie.style.display = "inline-block";
-  zigZane.style.display = "none";
+function disableOtherCharacter(display, notDisplay1, notDisplay2) {
+  display.style.display = "inline-block";
+  notDisplay1.style.display = "none";
+  notDisplay2.style.display = "none";
+}
+
+willieMenuImg.addEventListener("click", (e) => {
+  e.stopPropagation();
+  disableOtherCharacter(willie, roboSam, zigZane);
 });
 
-roboSamMenuImg.addEventListener("click", () => {
-  roboSam.style.display = "inline-block";
-  willie.style.display = "none";
-  zigZane.style.display = "none";
+function isUnlocked(char, button) {
+  if (char.unlocked) {
+    button.textContent = "Unlocked";
+    button.disable = true;
+  }
+}
+
+isUnlocked(characters.roboSamCharacter, roboSamBuyButton);
+isUnlocked(characters.zigZaneCharacter, zigZaneBuyButton);
+localStorage.setItem("multiplyer", characters.willieCharacter.coinsMultipliyer);
+
+// Character select menu, so that you can select a character
+willieMenuImg.addEventListener("click", (e) => {
+  e.stopPropagation();
+  disableOtherCharacter(willie, roboSam, zigZane);
+  localStorage.setItem(
+    "multiplyer",
+    characters.willieCharacter.coinsMultipliyer
+  );
 });
 
-zigZaneMenuImg.addEventListener("click", () => {
-  zigZane.style.display = "inline-block";
-  willie.style.display = "none";
-  roboSam.style.display = "none";
+roboSamMenuImg.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (characters.roboSamCharacter.unlocked) {
+    disableOtherCharacter(roboSam, willie, zigZane);
+    localStorage.setItem(
+      "multiplyer",
+      characters.roboSamCharacter.coinsMultipliyer
+    );
+  }
 });
 
-characterButton.addEventListener("click", () => {
+zigZaneMenuImg.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (characters.zigZaneCharacter.unlocked) {
+    disableOtherCharacter(zigZane, willie, roboSam);
+    console.log(characters.zigZaneCharacter.unlocked);
+    localStorage.setItem(
+      "multiplyer",
+      characters.zigZaneCharacter.coinsMultipliyer
+    );
+  }
+});
+
+characterButton.addEventListener("click", (e) => {
+  e.stopPropagation();
   startMenu.classList.add("displayNone");
   characterMenu.classList.add("active");
 });
 
 // Character back to start menu button
-characterBackBtn.addEventListener("click", () => {
+characterBackBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
   startMenu.classList.remove("displayNone");
   characterMenu.classList.remove("active");
 });
+
+// Coin/currency for the game
+chestBox.style.display = "none";
+openChest.style.display = "none";
+
+closedChest.addEventListener("click", (e) => {
+  e.stopPropagation();
+  openChest.style.display = "flex";
+  closedChest.style.display = "none";
+  chestBox.style.display = "flex";
+});
+
+openChest.addEventListener("click", (e) => {
+  e.stopPropagation();
+  chestBox.style.display = "none";
+  openChest.style.display = "none";
+  closedChest.style.display = "flex";
+});
+
+// Ingame currency system to make player have a goal of achiving a valueble that they then can buy something with
+
+coins.textContent = localStorage.getItem("coins");
+let coinRate = 10;
+let Multipliyer = Number(localStorage.getItem("multiplyer"));
+
+function getCoins(score) {
+  let coinsFromCurrentGame = Math.round(score / coinRate) * Multipliyer;
+
+  let currentCoins = Number(localStorage.getItem("coins"));
+
+  localStorage.setItem("coins", currentCoins + coinsFromCurrentGame);
+  coins.textContent = localStorage.getItem("coins");
+}
+
+function removeChestsFromScreen() {
+  closedChest.style.display = "none";
+  openChest.style.display = "none";
+}
 
 // Pause and start function to the game
 const play = document.createElement("i");
@@ -108,20 +265,24 @@ function addPauseAndStartButton() {
   pauseButton.style.display = "inline-block";
   play.style.display = "none";
 
-  pauseButton.addEventListener("click", () => {
+  pauseButton.addEventListener("click", (e) => {
+    e.stopPropagation();
     pauseButton.style.display = "none";
     play.style.display = "inline-block";
     obstacle.style.animationPlayState = "paused";
     stopScoreInterVal();
     triggerJump.style.animationPlayState = "paused";
+    backGroundMusic.pause();
   });
 
-  play.addEventListener("click", () => {
+  play.addEventListener("click", (e) => {
+    e.stopPropagation();
     play.style.display = "none";
     pauseButton.style.display = "inline-block";
     obstacle.style.animationPlayState = "running";
     triggerJump.style.animationPlayState = "running";
     startScoreInterval();
+    backGroundMusic.play();
   });
 }
 // Function to update the speed of the object comming towards the sprite
@@ -133,15 +294,23 @@ function checkCurrentScore(currentScore) {
   if (currentScore >= 0 && currentScore <= 20) {
     updateScoreTime = 2000;
     setAnimationSpeed(obstacle, 2);
+    obstacle.src = "pictures/New Piskel (19) (2).gif";
+    coinRate = 10;
   } else if (currentScore > 20 && currentScore <= 30) {
     updateScoreTime = 1800;
     setAnimationSpeed(obstacle, 1.8);
+    obstacle.src = "pictures/New Piskel (28).gif";
+    obstacle.style.left = 100 + "%";
+    coinRate = 9;
   } else if (currentScore > 30 && currentScore <= 50) {
     updateScoreTime = 1500;
     setAnimationSpeed(obstacle, 1.5);
+    obstacle.src = "pictures/New Piskel (27).gif";
+    coinRate = 8;
   } else if (currentScore > 50 && currentScore <= 80) {
     updateScoreTime = 1200;
     setAnimationSpeed(obstacle, 1.2);
+    coinRate = 7;
   }
 }
 
@@ -157,22 +326,35 @@ function stopScoreInterVal() {
 obstacle.classList.remove("animation");
 
 // The first screen u get to when page is loaded
-playBtn.addEventListener("click", () => {
+playBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
   obstacle.classList.add("animation");
   startMenu.classList.add("displayNone");
-
   startScoreInterval();
   addPauseAndStartButton();
+  playBackGroundMusic(backGroundMusic);
+  removeChestsFromScreen();
 });
 
 // event listener for space button down, if space is keydown it will jump and trigger an animation
+
+triggerJump.addEventListener("click", () => {
+  if (!sprite.classList.contains("jump")) {
+    sprite.classList.add("jump");
+    playSound(jumpSound);
+    jumpSound.volume = 0.05;
+
+    setTimeout(() => {
+      sprite.classList.remove("jump");
+    }, 1000);
+  }
+});
+
 triggerJump.addEventListener("keydown", (e) => {
-  if (e.key === " " || e.key === "Space") {
-    if (!sprite.classList.contains("jump")) {
-      sprite.classList.add("jump");
-      playSound(jumpSound);
-      jumpSound.volume = 0.05;
-    }
+  if (e.key === " " && !sprite.classList.contains("jump")) {
+    sprite.classList.add("jump");
+    playSound(jumpSound);
+    jumpSound.volume = 0.05;
 
     setTimeout(() => {
       sprite.classList.remove("jump");
@@ -181,7 +363,8 @@ triggerJump.addEventListener("keydown", (e) => {
 });
 
 // Play again button in the play again menu
-playAgainButton.addEventListener("click", () => {
+playAgainButton.addEventListener("click", (e) => {
+  e.stopPropagation();
   currentScore = 0;
   scoreEl.textContent = 0;
   obstacle.classList.add("animation");
@@ -192,6 +375,8 @@ playAgainButton.addEventListener("click", () => {
   hideMenu();
   addPauseAndStartButton();
   triggerJump.style.animationPlayState = "running";
+  playBackGroundMusic(backGroundMusic);
+  removeChestsFromScreen();
 });
 
 // Collision detection function
@@ -210,29 +395,17 @@ function checkCollision() {
     console.log("Collision detected!");
     obstacle.classList.remove("animation");
     obstacle.classList.add("resetPos");
+    highScoreData(currentScore);
     scoreDisplay.textContent = currentScore;
     play.style.display = "none";
     pauseButton.style.display = "none";
     triggerJump.style.animationPlayState = "paused";
+
+    closedChest.style.display = "flex";
+
     showMenu();
-
-    // Pushing current score while playing to a array and then making a new array with the highest scores
-    scores.push(currentScore);
-    let highestScore = 0;
-
-    // let highestScore = Math.max(...scores.slice(0, -1));
-
-    if (currentScore > highestScore) {
-      scoreDisplay.textContent = "NEW HIGH SCORE " + currentScore;
-      scoreDisplay.style.color = "gold";
-      playSound(newHighScoreSound);
-    } else {
-      playSound(deathsound);
-      deathsound.playbackRate = 1.2;
-      deathsound.volume = 0.2;
-    }
-
-    // Stoping the interval
+    backGroundMusic.pause();
+    getCoins(currentScore);
     stopScoreInterVal();
   }
   // Repeat on the next animation frame
